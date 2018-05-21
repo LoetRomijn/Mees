90109ff50e1797c93412a02cdb7bea9263bf2a3c
loet.romijn@finaps.nl <loet.romijn@finaps.nl>
2018-05-21 19:38:41 +0200

    initial files upload

---
+++ app.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+
+// 
+//
+// function changeBackgroundSmoothly() {
+//     $('.background').delay(5000).fadeTo(5000, 0 );
+// }
+//
+// changeBackgroundSmoothly();
+
+
+});
